32edc99373034a3fee0a43a27154c8f9b0cac2ad
hb00112 <159218387+hb00112@users.noreply.github.com>
2025-07-25 13:19:47 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -32,7 +32,7 @@ function formatDate(dateString) {
     const date = new Date(dateString);
     return date.toLocaleDateString('en-IN', {
         day: '2-digit',
-        month: 'short',
+        month: '2-digit',
         year: '2-digit'
     });
 }
